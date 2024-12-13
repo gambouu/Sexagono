026767a6cf86015d311a72d5962b3459c1b423bc
Marc Gamboa <160494647+gambouu@users.noreply.github.com>
2024-12-13 09:09:31 +0100

    Suida de archivos esqueleto

---
+++ src/edu/upc/epsevg/prop/hex/players/Sexagono.java
@@ -0,0 +1,31 @@
+package edu.upc.epsevg.prop.hex.players;
+
+import edu.upc.epsevg.prop.hex.HexGameStatus;
+import edu.upc.epsevg.prop.hex.IAuto;
+import edu.upc.epsevg.prop.hex.IPlayer;
+import edu.upc.epsevg.prop.hex.PlayerMove;
+import edu.upc.epsevg.prop.hex.PlayerType;
+import edu.upc.epsevg.prop.hex.SearchType;
+import java.awt.Point;
+
+/**
+ * AI player for the Hex game implementing the logic from the JavaScript code.
+ */
+public class Sexagono implements IPlayer, IAuto {
+
+    @Override
+    public PlayerMove move(HexGameStatus hgs) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void timeout() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public String getName() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+}
